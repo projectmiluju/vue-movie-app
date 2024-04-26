7f113255a5c4638a5f365fd34305bd8d4a058fd9
projectmiluju <dnjsdyd0712@naver.com>
2024-04-27 03:39:47 +0900

    무비 리스트 구현

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,38 @@
 import MovieHeader from './components/MovieHeader.vue';
 import MovieList from './components/MovieList.vue';
 import MovieFooter from './components/MovieFooter.vue';
+import { onMounted, ref } from 'vue';
+
+const movies = ref([]);
+
+onMounted(() => {
+  fetchMovieData();
+});
+
+function fetchMovieData() {
+  
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZGQ5MjlmZWMzZmIzOTE0OTQ3ZWUxNjM2YTllYjhhNSIsInN1YiI6IjY2MjdhMzdmMTc2YTk0MDE2NjgxOTAyYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.o5nxOyvekIumPVUX83BaMCf-dxv0GLuXwswlfY0Xd0w'
+  }
+};
+
+fetch('https://api.themoviedb.org/3/movie/now_playing?language=ko', options)
+  .then(response => response.json())
+  .then(response => {
+    console.log(response.results);
+    movies.value = response.results;
+  })
+  .catch(err => console.error(err));
+}
 
 </script>
 
 <template>
   <movie-header/>
-  <movie-list/>
+  <movie-list :movies="movies"/>
   <movie-footer/>
 </template>
 
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,14 +1,19 @@
 <script setup>
-  import MovieCard from './MovieCard.vue';
+import MovieCard from './MovieCard.vue';
+
+defineProps(['movies']);
+
 </script>
 
 <template>
     <main class="dark:bg-slate-700">
     <section class="max-w-7xl mx-auto py-7">
       <div class="flex justify-start flex-wrap">
-        <movie-card/>
-        <movie-card/>
-        <movie-card/>
+        <movie-card 
+          v-for="movie in movies"
+          :movie="movie"
+          :key="movie.id"
+        />
       </div>
     </section>
   </main>
