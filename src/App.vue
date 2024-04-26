1b6499de26da17bed8b18f2bd59f999a59acc5e2
projectmiluju <dnjsdyd0712@naver.com>
2024-04-27 03:10:59 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MovieHeader from './components/MovieHeader.vue';
+import MovieList from './components/MovieList.vue';
+import MovieFooter from './components/MovieFooter.vue';
+
+</script>
+
+<template>
+  <movie-header/>
+  <movie-list/>
+  <movie-footer/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import MovieCard from './MovieCard.vue';
+</script>
+
+<template>
+    <main class="dark:bg-slate-700">
+    <section class="max-w-7xl mx-auto py-7">
+      <div class="flex justify-start flex-wrap">
+        <movie-card/>
+        <movie-card/>
+        <movie-card/>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+</style>
